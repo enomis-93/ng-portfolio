7c783f88057f8fea803e63e0a2ba774d035224ac
Enomis9339 <61394403+Enomis9339@users.noreply.github.com>
2022-09-21 12:53:07 +0200

    add validation messagge & privacy policy checkbox

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -12,6 +13,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { ThemeService } from '../services/theme.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contacts',
@@ -19,7 +21,11 @@ import { ThemeService } from '../services/theme.service';
   styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
-  FormData!: FormGroup;
+  formGroup!: FormGroup;
+  submitted: boolean = false;
+  hasFormError: boolean = false;
+  emailPattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   whatsappLink: string =
     'https://wa.me/393383748792?text=Hi,%20I%20would%20contact%20you';
@@ -34,30 +40,59 @@ export class ContactsComponent implements OnInit {
 
   // isThemeDark: boolean = true;
 
-  constructor(
-    private builder: FormBuilder,
-    private themeService: ThemeService
-  ) {
-    // Subscribe to Theme Variable to catch theme changed
-    // this.themeService.themeChanged.subscribe((value) => {
-    //   this.isThemeDark = value;
-    // });
-  }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.FormData = this.builder.group({
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      message: new FormControl('', [Validators.required]),
+    this.formGroup = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+        ]),
+      ],
+      message: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(10)]),
+      ],
+      acceptTerms: [false, Validators.requiredTrue],
     });
   }
 
-  // Obtain theme variable from service
-  // get isDarkTheme(): boolean {
-  //   return this.themeService.isDarkModeActive;
-  // }
+  get form(): { [key: string]: AbstractControl } {
+    return this.formGroup.controls;
+  }
 
-  onSubmit(FormData: FormData) {
-    console.log(FormData);
+  get email() {
+    return this.formGroup.get('email');
+  }
+
+  get name() {
+    return this.formGroup.get('name');
+  }
+
+  get message() {
+    return this.formGroup.get('message');
+  }
+
+  onSubmit(form: FormGroup) {
+    this.submitted = true;
+    setInterval(() => {
+      this.submitted = false;
+    }, 3000);
+
+    if (form.valid) {
+      this.http
+        .post('https://formspree.io/f/xlevgoqz', form.value)
+        .subscribe((res) => console.log(res));
+      form.reset();
+    } else {
+      // Not-used for the moment because the send button is disabled until the form is invalid
+      this.hasFormError = true;
+    }
   }
 }
